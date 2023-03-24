339a0a897a0702c102773745b4c457614987229a
한 박 <djakrma0053@naver.com>
2023-03-24 15:29:56 +0900

    202303241529

---
+++ p202-ex1/MainApp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p202_ex1
+{
+    class MainApp
+    {
+        static void Dicide(int a, int b, out int quotient, out int remainder)
+        {
+            quotient = a / b;
+            remainder = a % b;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int a = 20;
+            int b = 3;
+            //int = c;
+            //int = d;
+
+            Dicide(a, b, out int c, out int d);
+
+            Console.WriteLine($"a:{a}, b:{b}, a/b:{c}, a%b{d}");
+        
+        }
+    }
+}
---
+++ p207_ex1/MainApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p207_ex1
+{
+    class MainApp
+    {
+
+        static int Sum(params int[] args)
+        {
+
+            Console.WriteLine("Suming....");
+            int sum = 0;
+
+            for (int i = 0; i < args.Length; i++)
+
+            {
+                if (i > 0)
+                        Console.WriteLine(",");
+
+                Console.WriteLine(args[i]);
+
+                sum += args[i];
+            }
+
+            Console.WriteLine();
+
+            return sum;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
