141eef66961428f7711bcee11d99f428bb1b5c6e
한 박 <djakrma0053@naver.com>
2023-03-27 14:58:41 +0900

    2023-03-27-1458

---
+++ p211_ex1/MainApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p211_ex1
+{
+    class MainApp
+    {
+
+        static void PrintProfilr(string name, string phone = "")
+        {
+
+            Console.WriteLine($"Name:(name), Phone:(phone)");
+        
+        }
+
+
+        static void Main(string[] args)
+        {
+            PrintProfilr("중근");
+            PrintProfilr("관순", "010-123-1234");
+            PrintProfilr(name: "봉길");
+            PrintProfilr("동주", phone: "010-789-7890");
+
+        }
+    }
+}
